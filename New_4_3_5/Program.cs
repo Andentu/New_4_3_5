5fc254504e59c38c01d5d3cf1c3fafa6f3d40831
Andentu <severalmaz97@gmail.com>
2023-02-13 20:20:11 +0300

    Добавлен код на вывод содержимого переменной name посимвольно через for & foreach. Добавлен вывод первого и последнего символа переменной.

--- New_4_3_5/Program.cs
+++ New_4_3_5/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.Write("Your name: " + name);
+
+            // цикл для вывода посимвольно переменной name (with for):
+            Console.Write("\nYour name by symbols (for): ");
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write(name[i] + " ");
+            }
+            Console.WriteLine("\nПервая буква вашего имени {0}", name[0]);
+
+            // цикл для вывода посимвольно переменной name (with foreach):
+            Console.Write("\nYour name by symbols (foreach): ");
+            foreach (var ch in name)
+            {
+                Console.Write(ch + " ");
+            }
+            Console.WriteLine("\nПоследняя буква вашего имени: {0}", name[name.Length - 1]);
+            Console.ReadKey(); 
+
+
         }
     }
 }
